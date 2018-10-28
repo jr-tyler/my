9ac11b26b43ae0a308ceac5e0e9e835adee0bd6a
tylerj <ropertylerj@gmail.com>
2018-10-29 11:47:21 +1300

    Updated static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
